654cedf6a0e9106950071702cd059ebcca1eda05
MHL289 <herreraarrieta400@gmail.com>
2025-05-29 08:46:52 -0500

    Nuevas escenas
    Hace varias cosas como la del repositorio avanzado

---
+++ RVProject/Assets/Scripts/CambiarEscenaConRayo.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string sceneToLoad; // Nombre de la escena a cargar
+
+    public void ChangeScene(XRBaseInteractor interactor)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
